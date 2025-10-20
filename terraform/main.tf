abbf7f73ccca8c204cf0ae198c6dab0adad801c3
John McDonough <movinalot@gmail.com>
2025-10-20 15:46:37 -0400

    update workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,15 +41,13 @@ data "fortiflexvm_entitlements_list" "entitlements_list" {
   serial_number = each.value.fortiflex_serial
 }
 
-resource "fortiflexvm_entitlements_vm" "entitlements_vm" {
-  for_each = var.fortiflex_serial_numbers
+# resource "fortiflexvm_entitlements_vm" "entitlements_vm" {
+#   for_each = var.fortiflex_serial_numbers
 
-  config_id     = data.fortiflexvm_entitlements_list.entitlements_list[each.key].entitlements[0].config_id
-  serial_number = data.fortiflexvm_entitlements_list.entitlements_list[each.key].entitlements[0].serial_number
-  status        = "ACTIVE"
-}
-
-
+#   config_id     = data.fortiflexvm_entitlements_list.entitlements_list[each.key].entitlements[0].config_id
+#   serial_number = data.fortiflexvm_entitlements_list.entitlements_list[each.key].entitlements[0].serial_number
+#   status        = "ACTIVE"
+# }
 
 resource "fortiflexvm_entitlements_vm_token" "entitlements_vm_token" {
   for_each = var.fortiflex_serial_numbers
@@ -59,9 +57,9 @@ resource "fortiflexvm_entitlements_vm_token" "entitlements_vm_token" {
   regenerate_token = data.fortiflexvm_entitlements_list.entitlements_list[each.key].entitlements[0].token_status == "USED" && data.fortiflexvm_entitlements_list.entitlements_list[each.key].entitlements[0].status == "ACTIVE" ? false : true
 }
 
-output "entitlements_vms" {
-  value = fortiflexvm_entitlements_vm.entitlements_vm[*]
-}
+# output "entitlements_vms" {
+#   value = fortiflexvm_entitlements_vm.entitlements_vm[*]
+# }
 
 output "entitlements_vm_token" {
   value = fortiflexvm_entitlements_vm_token.entitlements_vm_token
