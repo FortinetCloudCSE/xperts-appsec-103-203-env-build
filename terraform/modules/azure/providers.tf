7385747c7338fe6088d1b3a7de47104f757ee5b9
John McDonough <movinalot@gmail.com>
2026-06-10 18:18:17 -0400

    update providers

--- terraform/modules/azure/providers.tf
+++
@@ -1,5 +0,0 @@
-provider "fortiflexvm" {
-  # FortiFLEX VM provider configuration username and password are pulled from environment variables
-  username = var.fortiflex_access_username
-  password = var.fortiflex_access_password
-}
